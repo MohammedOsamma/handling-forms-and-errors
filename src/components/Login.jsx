c5058f062606e41436d597eaf2b52882e30ecaa2
MohammedOsamma <mo856915i@gmail.com>
2025-04-22 19:07:36 +0200

    Manging & Getting User Input Via State

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [emailEnterd, setEnteredEmail] = useState("");
+  const [passwordEntered, setEnteredPassword] = useState("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("email:", emailEnterd);
+    console.log("Password:", passwordEntered);
+  }
+
+  function emailHanlder(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function passwordHanlder(event) {
+    setEnteredPassword(event.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={emailHanlder}
+            value={emailEnterd}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={passwordHanlder}
+            value={passwordEntered}
+          />
         </div>
       </div>
 
